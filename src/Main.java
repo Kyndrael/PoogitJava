b8aeee9345e8f0ac5702fa4e71723731772604e2
Kyndrael <jonathansilveira95@hotmail.com>
2026-08-22 12:30:59 -0300

    Implement main functionality for IPhone operations

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,31 @@
+import model.IPhone;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        IPhone iphone = new IPhone();
+
+        System.out.println("=== REPRODUTOR MUSICAL ===");
+
+        iphone.selecionarMusica("Imagine - John Lennon");
+        iphone.tocar();
+        iphone.pausar();
+
+        System.out.println();
+
+        System.out.println("=== APARELHO TELEFÔNICO ===");
+
+        iphone.ligar("21999999999");
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+
+        System.out.println();
+
+        System.out.println("=== NAVEGADOR NA INTERNET ===");
+
+        iphone.exibirPagina("https://www.google.com");
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+    }
+}
